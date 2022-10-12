30668f8c6aeeee5c1e3ee61398e0ac39cd15d9b2
Julio Cesar <juljunior1@gmail.com>
2022-10-12 08:27:30 -0500

    Se considera trabajar la apikey como instancia de .env

--- api/src/db.js
+++ api/src/db.js
@@ -3,9 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, API_KEY
 } = process.env;
 
+console.log(`Este es mi KEY ${API_KEY} y mi user ${DB_USER}`);
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -27,7 +28,7 @@ modelDefiners.forEach(model => model(sequelize));
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
-
+console.log(sequelize.models)
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Recipe, Diet } = sequelize.models;
@@ -39,4 +40,5 @@ Recipe.belongsToMany(Diet, {through:"recipe_diet"});
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  API_KEY
 };
