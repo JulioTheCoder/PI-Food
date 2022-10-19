843b1580ee2952fc837e1f8fcb4c8cfa09bb4f4a
Julio Cesar <juljunior1@gmail.com>
2022-10-18 20:23:16 -0500

    Filtros y ordenamientos listos

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -15,6 +15,7 @@ export default function Home(){
   const allDiets = useSelector((state) => state.diets)
   const [currentPage, setCurrentPage] = useState(1);
   const [ recipesPerPage, setReciPeperPage] = useState(9);
+  const [ order, setOrder ] = useState("")
   const indexOfLastRecipe = currentPage * recipesPerPage;
   const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
   const currentRecipe = allRecipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
@@ -39,6 +40,35 @@ export default function Home(){
     dispatch(action.filterRecipeByDiet(e.target.value))
   }
 
+  function handlerFilterApi_Created(e){
+    e.preventDefault();
+    dispatch(action.filterRecipeApiOCreado(e.target.value));
+  }
+
+  function handlerOderByTitle(e) {
+    if (e.target.value === "default") {
+      handleClick(e);
+    } else {
+      e.preventDefault();
+      dispatch(action.orderByName(e.target.value));
+      setCurrentPage(1);
+      setOrder(`Ordenado ${e.target.value}`);
+    }
+    
+  }
+
+  function handlerOderByHealthScoer(e) {
+    if (e.target.value === "default") {
+      handleClick(e);
+    } else {
+      e.preventDefault();
+      dispatch(action.orderByHealthScore(e.target.value));
+      setCurrentPage(1);
+      setOrder(`Ordenado ${e.target.value}`);
+    }
+    
+  }
+
   return (
     <div>
       <Link to={"/recipe"}></Link>
@@ -46,24 +76,24 @@ export default function Home(){
       <button onClick={e => handleClick(e)}>Refresh</button>
 
       <div>
-        <select>
-          <option value={"default"}>Nombre</option>
+        <select onChange={(e) => handlerOderByTitle(e)}>
+          <option value={"default"}>Title</option>
           <option value={"asc"}>Ascendente</option>
-          <option value={"desc"}>Descendente</option>
+          <option value={"des"}>Descendente</option>
         </select>
 
-        <select>
-          <option value={"default"}>Punto de salud</option>
+        <select onChange={(e) => handlerOderByHealthScoer(e)}>
+          <option value={"default"}>Health Score</option>
           <option value={"asc"}>Ascendente</option>
-          <option value={"desc"}>Descendente</option>
+          <option value={"des"}>Descendente</option>
         </select>
 
         <select onChange={(e) => handlerFilterByDiet(e)}>
-          <option value={"default"}>Dieta</option>
+          <option value={"all"}>Diet</option>
           <SelectOptions nameOptions={allDiets} />
         </select>
 
-        <select>
+        <select onChange={(e) => handlerFilterApi_Created(e)}>
           <option value={"all"}>Api/Ceated</option>
           <option value={"api"}>Api</option>
           <option value={"created"}>Created</option>
--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -21,21 +21,92 @@ function rootReducer(state = initialState, action){
         diets: action.payload
       }
     case "FILTER_BY_DIET": 
-      const allRecipesFBD = state.recipes;
+      const allRecipesFBD = state.allRecipes;
       //console.log("Paiload: ",action.payload)
-      const statusFiltered = action.payload === "Todo"? allRecipesFBD: allRecipesFBD.filter(el => el.diets.includes(action.payload));
-      
+      const statusFiltered = action.payload === "all"? allRecipesFBD: allRecipesFBD.filter(el => el.diets.includes(action.payload));
+      statusFiltered.length === 0 && alert(`No hay resultados para: ${action.payload}`);
+
         return{
           ...state,
-          recipes:statusFiltered
+          recipes:statusFiltered.length?statusFiltered:allRecipesFBD
         }
 
     case "FILTER_BY_API_CREADO":
-      const allVideogamesFBAC = state.allVideogames;
-      const apiOCreado = action.payload === "Api"? allVideogamesFBAC.filter(el => !(el.created)) : allVideogamesFBAC.filter(el => el.created);
+      const allRecipesFBAC = state.allRecipes;
+      const apiOCreado = action.payload === "api"? allRecipesFBAC.filter(el => !(el.created)) : allRecipesFBAC.filter(el => (el.created));
+      apiOCreado.length === 0 && action.payload !== "all" && alert(`No hay filtro por: ${action.payload}`) 
       return{
         ...state,
-        videogames:action.payload === "Todo"?allVideogamesFBAC:apiOCreado
+        recipes:action.payload === "all"?allRecipesFBAC:apiOCreado.length === 0?allRecipesFBAC:apiOCreado
+      }
+
+    case "ORDER_BY_NAME":
+      let ordenNameArr = action.payload === "asc"?
+      state.recipes.sort(function(a, b){
+        if (a.name > b.name) {
+          return 1
+        }
+        if(a.name < b.name){
+          return -1;
+        }
+        return 0;
+      }):action.payload === "des"?
+      
+      state.recipes.sort(function(a, b){
+        if (a.name > b.name) {
+          return -1;
+        }
+        if(a.name < b.name){
+          return 1;
+        }
+        return 0
+      }):state.allRecipes
+      
+      return{
+        ...state,
+        recipes:ordenNameArr
+      }
+  
+    case "ORDER_BY_HEALTHSCORE":
+      let ordenRatingArr = action.payload === "asc"?
+      state.recipes.sort(function(a, b){
+        if (a.healthScore > b.healthScore) {
+          return 1
+        }
+        if(a.healthScore < b.healthScore){
+          return -1;
+        }
+        return 0;
+      }):action.payload === "des"?
+      
+      state.recipes.sort(function(a, b){
+        if (a.healthScore > b.healthScore) {
+          return -1;
+        }
+        if(a.healthScore < b.healthScore){
+          return 1;
+        }
+        return 0
+      }):state.allRecipes
+      
+      return{
+        ...state,
+        recipes:ordenRatingArr
+      }
+
+    case "GET_VIDEOGAME_BY_NAME":
+      return{
+        ...state,
+        videogames:action.payload
+      }
+    case "POST_VIDEOGAME":
+      return{
+        ...state
+      }
+    case "GET_DETAILS":
+      return{
+        ...state,
+        details:action.payload
       }
 
     default:
